58f2302904f9d2edd62f5d3014c063a8234a0872
KatelevskiyViktor <empirikk@ya.ru>
2022-11-23 17:59:42 +0300

    Part2, OOP, sem2 HW

--- oop/sem1_2HW/task2/Wardrobe.java
+++ oop/sem1_2HW/task2/Wardrobe.java
@@ -7,7 +7,7 @@ public class Wardrobe {
     public static String[] clothes = new String[5];
 
     @Override
-    public String toString() {
+    public String toString() {       
         return "In wardrobe is: " + Arrays.toString(clothes);
     }
 }
